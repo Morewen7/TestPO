14bb5858d307fb7bb5bc24394b1553e3d2893c63
Morewen7 <93655709+Morewen7@users.noreply.github.com>
2022-04-22 12:38:10 +0300

    done

---
+++ Test/src/test/Main.java
@@ -0,0 +1,16 @@
+package test;
+
+
+import org.junit.Assert;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        test1 calculator = new test1();
+        int a = test1.displayInfo(3, 4);
+        System.out.println(a);
+        Object Assert = new Object();
+        Assert.wait(7, a);
+
+
+    }
+}
